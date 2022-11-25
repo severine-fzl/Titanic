0208ca6577920c32ef5b182c4d0a8fa90946913d
Severine <82463456+severine-fzl@users.noreply.github.com>
2022-11-25 16:46:03 +0100

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -3,12 +3,3 @@ import crypto from 'crypto';
 export function sha256(value, secret = process.env.APP_HASH_SALT) {
   return crypto.createHmac('sha256', secret).update(value).digest('hex');
 }
-
-/* export function compare(value, hash) {
-  return sha256(value) === hash;
-} */
-
-
-
-//bycript
-//argon2id
